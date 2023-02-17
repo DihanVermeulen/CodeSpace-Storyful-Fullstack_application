0242b74ee53aba259975e828f802ab1e0ae6685d
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-17 11:06:56 +0200

    Inserted bottom tab navigator into root

--- Storyful-client/src/root.tsx
+++ Storyful-client/src/root.tsx
@@ -1,11 +1,13 @@
 // Root components are placed here.
 
 import RootHeader from "./components/layout/RootHeader";
+import BottomTabNavigator from "./components/navigation/BottomTabNavigator";
 
 const Root = () => {
   return (
     <>
       <RootHeader />
+      <BottomTabNavigator />
     </>
   );
 };
