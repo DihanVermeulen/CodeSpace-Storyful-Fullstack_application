432a011857541294557a9bb4552efb65655b6eca
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-17 14:27:14 +0200

    Added icons to containers

--- Storyful-client/src/components/navigation/BottomTabNavigator/BottomTabNavigator.tsx
+++ Storyful-client/src/components/navigation/BottomTabNavigator/BottomTabNavigator.tsx
@@ -1,5 +1,22 @@
+import "./BottomTabNavigator.css";
+import homeIcon from "./../../../assets/BottomTabNavigatorIcons/home-icon.svg";
+import bookIcon from "./../../../assets/BottomTabNavigatorIcons/book-icon.svg";
+import profileIcon from "./../../../assets/BottomTabNavigatorIcons/profile-icon.svg";
+
 const BottomTabNavigator = () => {
-  return <nav className="bottomtabnavigator-container"></nav>;
+  return (
+    <nav className="bottomtabnavigator-container">
+      <div className="bottomtabnavigator-container-button-container">
+        <img src={homeIcon} />
+      </div>
+      <div className="bottomtabnavigator-container-button-container">
+        <img src={bookIcon} />
+      </div>
+      <div className="bottomtabnavigator-container-button-container">
+        <img src={profileIcon} />
+      </div>
+    </nav>
+  );
 };
 
 export default BottomTabNavigator;
