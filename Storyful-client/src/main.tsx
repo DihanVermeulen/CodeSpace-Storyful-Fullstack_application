2e82fd3233234164e3b68b87c68ea42874e942d9
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-17 13:46:24 +0200

    Added base children paths to root

--- Storyful-client/src/main.tsx
+++ Storyful-client/src/main.tsx
@@ -3,11 +3,28 @@ import ReactDOM from "react-dom/client";
 import Root from "./root";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./routes/Home";
+import Library from "./routes/Library";
+import Profile from "./routes/Profile";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "library",
+        element: <Library />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
   },
 ]);
 
