e4edc54dc6516b6ce8a1cb1231816af81201573d
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-17 14:08:31 +0200

    Updated import

--- Storyful-client/src/root.tsx
+++ Storyful-client/src/root.tsx
@@ -1,7 +1,7 @@
 // Root components are placed here.
 
 import RootHeader from "./components/layout/RootHeader";
-import BottomTabNavigator from "./components/navigation/BottomTabNavigator";
+import BottomTabNavigator from "./components/navigation/BottomTabNavigator/BottomTabNavigator";
 import { Outlet } from "react-router-dom";
 
 const Root = () => {
