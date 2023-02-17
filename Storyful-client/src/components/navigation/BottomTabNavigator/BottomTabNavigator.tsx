b1ec406729e8e74acc482871b3d3d636abf8c2f3
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-17 14:31:07 +0200

    Created function for handling tab clicks

--- Storyful-client/src/components/navigation/BottomTabNavigator/BottomTabNavigator.tsx
+++ Storyful-client/src/components/navigation/BottomTabNavigator/BottomTabNavigator.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import "./BottomTabNavigator.css";
 import homeIcon from "./../../../assets/BottomTabNavigatorIcons/home-icon.svg";
 import bookIcon from "./../../../assets/BottomTabNavigatorIcons/book-icon.svg";
 import profileIcon from "./../../../assets/BottomTabNavigatorIcons/profile-icon.svg";
 
 const BottomTabNavigator = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabClick = (tabIndex: number) => {
+    setActiveTab(tabIndex);
+  };
+
   return (
     <nav className="bottomtabnavigator-container">
       <div className="bottomtabnavigator-container-button-container">
