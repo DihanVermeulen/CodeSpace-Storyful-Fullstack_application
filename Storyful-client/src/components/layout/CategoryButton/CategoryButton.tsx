532676fb7192b90ae75d9fea009a210bc0bd9ff7
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-19 08:17:42 +0200

    Imported css file and added class to component container

--- Storyful-client/src/components/layout/CategoryButton/CategoryButton.tsx
+++ Storyful-client/src/components/layout/CategoryButton/CategoryButton.tsx
@@ -1,11 +1,13 @@
+import "./CategoryButton.css";
+
 interface Props {
-    title: string;
-    filter: any;
-    icon: any;
+  title: string;
+  filter: any;
+  icon: any;
 }
 
-const CategoryButton: React.FC<Props> = () => {
-  return <></>;
+const CategoryButton: React.FC<Props> = ({ title, filter, icon }) => {
+  return <div className="category-button"></div>;
 };
 
 export default CategoryButton;
