768ed1f0e9ee7dec57fdb48eec78b7059ef1e7ac
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-19 08:26:58 +0200

    Added category button to Home component

--- Storyful-client/src/routes/Home/Home.tsx
+++ Storyful-client/src/routes/Home/Home.tsx
@@ -1,3 +1,4 @@
+import CategoryButton from "../../components/layout/CategoryButton/CategoryButton";
 import Searchbar from "../../components/layout/Searchbar/Searchbar";
 import "./Home.css";
 
@@ -6,6 +7,12 @@ const Home = () => {
     <section className="home-container">
       <Searchbar />
       <h2 className="home-category-heading">Categories</h2>
+      <article className="home-container-category-container">
+        <CategoryButton title="Action" />
+        <CategoryButton title="Comedy" />
+        <CategoryButton title="Horror" />
+        <CategoryButton title="Romance" />
+      </article>
     </section>
   );
 };
