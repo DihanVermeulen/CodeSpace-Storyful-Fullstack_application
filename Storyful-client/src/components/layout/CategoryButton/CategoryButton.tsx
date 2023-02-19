d463be4b6e11f138afd235c90065ad79620d8ce2
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-19 08:27:42 +0200

    Updated

--- Storyful-client/src/components/layout/CategoryButton/CategoryButton.tsx
+++ Storyful-client/src/components/layout/CategoryButton/CategoryButton.tsx
@@ -2,19 +2,19 @@ import "./CategoryButton.css";
 
 interface Props {
   title: string;
-  filter: any;
-  icon: any;
+  filter?: any;
+  icon?: any;
 }
 
 const CategoryButton: React.FC<Props> = ({ title, filter, icon }) => {
-  return <div className="category-button">
-    <div>
-
-    </div>
-    <div>
+  return (
+    <div className="category-button">
+      <div></div>
+      <div>
         <h4 className="category-button-title">{title}</h4>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default CategoryButton;
