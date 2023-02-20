3aa89cfadae4ce8aac9fa33bd5c5015a26c7f8b8
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-20 08:00:06 +0200

    Changed class names to fit new styles

--- Storyful-client/src/components/layout/CategoryButton/CategoryButton.tsx
+++ Storyful-client/src/components/layout/CategoryButton/CategoryButton.tsx
@@ -9,9 +9,11 @@ interface Props {
 const CategoryButton: React.FC<Props> = ({ title, filter, icon }) => {
   return (
     <div className="category-button">
-      <div></div>
-      <div>
-        <h4 className="category-button-title">{title}</h4>
+      <div className="category-button-group">
+        <div className="category-button-item">{icon}</div>
+        <div className="category-button-item">
+          <h4 className="category-button-title">{title}</h4>
+        </div>
       </div>
     </div>
   );
