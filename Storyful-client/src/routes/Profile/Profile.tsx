cc18435e2af480b8dc53e087c798e5eee29d0603
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-20 11:22:20 +0200

    Added stats section with three columns

--- Storyful-client/src/routes/Profile/Profile.tsx
+++ Storyful-client/src/routes/Profile/Profile.tsx
@@ -5,6 +5,8 @@ import "./Profile.css";
 const Profile = () => {
   const [userName, setUserName] = useState<string>("Guest");
   const [storiesRead, setStoriesRead] = useState<number>(0);
+  const [storiesInLibrary, setStoriesInLibrary] = useState<number>(0);
+  const [storiesReading, setStoriesReading] = useState<number>(0);
 
   return (
     <section className="profile-container">
@@ -16,7 +18,24 @@ const Profile = () => {
       </section>
       <article className="profile-container-user-stats">
         <div className="profile-container-user-stats-item">
-          <p>{storiesRead}</p>
+          <p className="profile-container-user-stats-item-number">
+            {storiesRead}
+          </p>
+          <h3 className="profile-container-user-stats-item-title">read</h3>
+        </div>
+        <div className="profile-container-user-stats-item">
+          <p className="profile-container-user-stats-item-number">
+            {storiesInLibrary}
+          </p>
+          <h3 className="profile-container-user-stats-item-title">
+            in library
+          </h3>
+        </div>
+        <div className="profile-container-user-stats-item">
+          <p className="profile-container-user-stats-item-number">
+            {storiesReading}
+          </p>
+          <h3 className="profile-container-user-stats-item-title">reading</h3>
         </div>
       </article>
     </section>
