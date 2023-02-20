0379aaaeca7c4d990cbe9da4f478c07913782467
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-20 10:19:10 +0200

    Created basic structure to page

--- Storyful-client/src/routes/Profile/Profile.tsx
+++ Storyful-client/src/routes/Profile/Profile.tsx
@@ -1,7 +1,14 @@
+import BookShelfImage from "./../../assets/profile/bookshelf.png";
 import "./Profile.css";
 
 const Profile = () => {
-  return <div>profile</div>;
+  return (
+    <section className="profile-container">
+      <section className="profile-top-section">
+        <div className="element"></div>
+      </section>
+    </section>
+  );
 };
 
 export default Profile;
