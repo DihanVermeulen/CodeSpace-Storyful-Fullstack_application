24745dc037a086738ef9562602b005ea27f176df
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-20 10:18:43 +0200

    Removed separator

--- Storyful-client/src/routes/Library/Library.tsx
+++ Storyful-client/src/routes/Library/Library.tsx
@@ -6,7 +6,6 @@ const Library = () => {
     <section className="library-container">
       <h1 className="library-heading">Library</h1>
       <Searchbar />
-      <div style={{marginTop: 10, marginBottom: 20}} className="separator"/>
     </section>
   );
 };
