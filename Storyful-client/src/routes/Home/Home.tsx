f4b59e2002b41ea2b76dec91274e0608582b112d
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-20 08:16:02 +0200

    Added icons to category buttons

--- Storyful-client/src/routes/Home/Home.tsx
+++ Storyful-client/src/routes/Home/Home.tsx
@@ -1,6 +1,10 @@
 import CategoryButton from "../../components/layout/CategoryButton/CategoryButton";
 import Searchbar from "../../components/layout/Searchbar/Searchbar";
 import "./Home.css";
+import ComedyIcon from "./../../assets/Categories/ComedyIcon";
+import { HorrorIcon } from "../../assets/Categories/HorrorIcon";
+import RomanceIcon from "../../assets/Categories/RomanceIcon";
+import ActionIcon from "../../assets/Categories/ActionIcon";
 
 const Home = () => {
   return (
@@ -8,10 +12,13 @@ const Home = () => {
       <Searchbar />
       <h2 className="home-category-heading">Categories</h2>
       <article className="home-container-category-container">
-        <CategoryButton title="Action" />
-        <CategoryButton title="Comedy" />
-        <CategoryButton title="Horror" />
-        <CategoryButton title="Romance" />
+        <CategoryButton title="Action" icon={<ActionIcon colour="#000000" />} />
+        <CategoryButton title="Comedy" icon={<ComedyIcon colour="#000000" />} />
+        <CategoryButton title="Horror" icon={<HorrorIcon colour="#000000" />} />
+        <CategoryButton
+          title="Romance"
+          icon={<RomanceIcon colour="#000000" />}
+        />
       </article>
     </section>
   );
