3c7b0cd7a2ffd653dbeea779155bbe567fc8e10c
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-20 11:12:52 +0200

    Added section to display user name and user stats

--- Storyful-client/src/routes/Profile/Profile.tsx
+++ Storyful-client/src/routes/Profile/Profile.tsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import BookShelfImage from "./../../assets/profile/bookshelf.png";
 import "./Profile.css";
 
 const Profile = () => {
+  const [userName, setUserName] = useState<string>("Guest");
+  const [storiesRead, setStoriesRead] = useState<number>(0);
+
   return (
     <section className="profile-container">
       <section className="profile-top-section">
-        <div className="element"></div>
+        <div className="profile-image"></div>
       </section>
+      <section className="profile-container-user-information">
+        <h3>{userName}</h3>
+      </section>
+      <article className="profile-container-user-stats">
+        <div className="profile-container-user-stats-item">
+          <p>{storiesRead}</p>
+        </div>
+      </article>
     </section>
   );
 };
