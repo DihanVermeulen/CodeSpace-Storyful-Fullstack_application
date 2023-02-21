85ed74fe57f3b2bf60e8f041c28aa16b15b7ecd5
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-21 14:09:52 +0200

    Added DI container to AppFactory

--- Storyful-API/bootstrap/index.php
+++ Storyful-API/bootstrap/index.php
@@ -1,11 +1,15 @@
 <?php
 
+use DI\Container;
 use Slim\Factory\AppFactory;
 use Psr\http\Message\ResponseInterface as Response;
 use Psr\http\Message\RequestInterface as Request;
 
 require __DIR__ . './../vendor/autoload.php';
 
+$container = new Container();
+
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response) {
