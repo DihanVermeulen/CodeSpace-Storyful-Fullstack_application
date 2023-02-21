6a2d17511f4832c06513acddf0cf93cedf29f759
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-21 14:43:49 +0200

    Adds middleware and settings to app

--- Storyful-API/bootstrap/index.php
+++ Storyful-API/bootstrap/index.php
@@ -6,13 +6,16 @@ use Psr\http\Message\ResponseInterface as Response;
 use Psr\http\Message\RequestInterface as Request;
 
 require __DIR__ . './../vendor/autoload.php';
-$settings = require __DIR__ . './../app/settings.php';
 
+$settings = require __DIR__ . './../app/settings.php';
+$middleware = require __DIR__ . './../app/middleware.php';
 $container = new Container;
 
 AppFactory::setContainer($container);
+
 $app = AppFactory::create();
-$app->addErrorMiddleware(true, true, true);
+$middleware($app);
+$settings($container);
 
 $app->get('/', function (Request $request, Response $response) {
     $response->getBody()->write('Hello World!');
