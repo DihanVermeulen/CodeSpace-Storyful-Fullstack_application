924e642a4c638d345644fdd561bba6e4b4bc08a3
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-21 14:53:41 +0200

    Added routes

--- Storyful-API/bootstrap/index.php
+++ Storyful-API/bootstrap/index.php
@@ -5,16 +5,19 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . './../vendor/autoload.php';
 
-$settings = require __DIR__ . './../app/settings.php';
-$middleware = require __DIR__ . './../app/middleware.php';
 $container = new Container;
 
 AppFactory::setContainer($container);
 
 $app = AppFactory::create();
+
+$middleware = require __DIR__ . './../app/middleware.php';
 $middleware($app);
+
+$settings = require __DIR__ . './../app/settings.php';
 $settings($container);
 
-
+$routes = require __DIR__ . './../app/routes.php';
+$routes($app);
 
 $app->run();
