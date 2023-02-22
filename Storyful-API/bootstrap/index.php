b76feb4892c696b1d619faa6e9c7b76948b8cd3c
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-22 13:44:26 +0200

    Fixed problem with middleware not working due to settings not being instansiated first

--- Storyful-API/bootstrap/index.php
+++ Storyful-API/bootstrap/index.php
@@ -1,7 +1,6 @@
 <?php
 
 use DI\Container;
-// use Slim\Factory\AppFactory;
 use App\Models\User;
 use \DI\Bridge\Slim\Bridge as SlimAppFactory;
 
@@ -11,11 +10,11 @@ $container = new Container;
 
 $app = SlimAppFactory::create($container);
 
-$middleware = require __DIR__ . './../app/middleware.php';
-$middleware($app);
-
 $settings = require __DIR__ . './../app/settings.php';
 $settings($container);
+
+$middleware = require __DIR__ . './../app/middleware.php';
+$middleware($app);
 
 $routes = require __DIR__ . './../app/routes.php';
 $routes($app);
