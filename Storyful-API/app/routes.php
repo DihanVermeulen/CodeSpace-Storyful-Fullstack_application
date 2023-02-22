fac5413b751d4ab0ee7a0e551e9972e27e24d36a
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-22 05:57:33 +0200

    Added group for users route and added UserController

--- Storyful-API/app/routes.php
+++ Storyful-API/app/routes.php
@@ -1,13 +1,15 @@
 <?php
 
 use Slim\App;
+use App\Http\Controllers\UserController;
 use Psr\http\Message\ResponseInterface as Response;
 use Psr\http\Message\RequestInterface as Request;
 
 return function(App $app) {
-    $app->get('/', function (Request $request, Response $response) {
-        $response->getBody()->write('Hello World!');
-        return $response;
+    // $app->get('/users', [UserController::class, 'findAll']);
+
+    $app->group('/users', function() use ($app) {
+        $app->get('', [UserController::class, 'findAll']);
     });
 }
 
