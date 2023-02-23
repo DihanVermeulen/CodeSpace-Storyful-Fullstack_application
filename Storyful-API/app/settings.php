a2dc79060a519277e02a598bf23228e53bc64472
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-23 09:03:53 +0200

    Creates connection to MySQL database

--- Storyful-API/app/settings.php
+++ Storyful-API/app/settings.php
@@ -1,13 +1,35 @@
 <?php 
 
-use Psr\Container\ContainerInterface;
-
 return function($container) {
     $container->set('settings', function() {
-        return ['displayErrorDetails' => true,
+        return [
+        'displayErrorDetails' => true,
         'logErrorDetails' => true,
-        'logErrors' => true
+        'logErrors' => true,
+        'db' => [
+            'host' => 'localhost',
+            'dbname' => 'storyful_db',
+            'user' => 'root',
+            'password' => '',
+            'charset' => 'utf8mb4'
+        ],
     ];
+    });
+
+    $container->set('db', function ($container) {
+        $settings = $container->get('settings')['db'];
+    
+        $host = $settings['host'];
+        $dbname = $settings['dbname'];
+        $user = $settings['user'];
+        $password = $settings['password'];
+        $charset = $settings['charset'];
+    
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+    
+        $db = new \PDO($dsn, $user, $password);
+    
+        return $db;
     });
 }
 
