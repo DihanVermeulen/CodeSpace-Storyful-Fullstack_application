1e357ce31db0b1cec54106412f334c2326411316
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-23 09:04:26 +0200

    Instantiated UserController class

---
+++ Storyful-API/app/Http/controllers/UserController.php
@@ -0,0 +1,7 @@
+namespace App\Http\Controllers;
+use Psr\Container\ContainerInterface;
+use Psr\http\Message\ResponseInterface as Response;
+use Psr\http\Message\RequestInterface as Request;
+class UserController
+{
+}
