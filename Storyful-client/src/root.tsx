48d7c50de4883b46b8dbbb021cd212f9e95678aa
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-23 14:50:44 +0200

    Fetches all stories from database

--- Storyful-client/src/root.tsx
+++ Storyful-client/src/root.tsx
@@ -3,12 +3,22 @@
 import RootHeader from "./components/layout/RootHeader/RootHeader";
 import BottomTabNavigator from "./components/navigation/BottomTabNavigator/BottomTabNavigator";
 import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { fetchAllStoriesFromDatabase } from "./utils/helpers";
 
 const Root = () => {
+  const [stories, setStories] = useState<any>();
+
+  useEffect(() => {
+    fetchAllStoriesFromDatabase().then((response: any) =>
+      setStories(response.data)
+    );
+  }, []);
+
   return (
     <>
       <RootHeader />
-      <Outlet />
+      <Outlet context={stories} />
       <BottomTabNavigator />
     </>
   );
