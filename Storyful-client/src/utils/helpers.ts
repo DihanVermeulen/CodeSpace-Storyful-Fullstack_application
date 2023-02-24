b077ec39e65ec8f6ab1283e25abc6f3879acb77a
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-24 14:05:36 +0200

    Changed extension to ts

---
+++ Storyful-client/src/utils/helpers.ts
@@ -0,0 +1,20 @@
+import axios from "../services/axios/axios";
+import StoriesTestData from '../data/__tests__/stories.json';
+
+export const fetchAllStoriesFromDatabase = async () => {
+  try {
+    const response = await axios.get("/stories");
+    return response;
+  } catch (error: any) {
+    throw new Error(error.response.data.error);
+  }
+};
+
+export const fetchAllStoriesFromDatabaseTest = async () => {
+  try {
+    const response = await StoriesTestData;
+    return response;
+  } catch (error: any) {
+    throw new Error(error.response.data.error);
+  }
+};
