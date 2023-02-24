b06edc699c46d07064ae6b897726a95f2249f0b0
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-24 17:07:52 +0200

    Created @types folder and added stories interface as well as context type

---
+++ Storyful-client/src/@types/stories.d.ts
@@ -0,0 +1,12 @@
+export interface IStories {
+  id: number;
+  author: string;
+  title: string;
+  genre: string;
+  cover_location: string;
+}
+
+export type storiesContextType = {
+  stories: IStories[];
+  fetchStories: () => void;
+};
