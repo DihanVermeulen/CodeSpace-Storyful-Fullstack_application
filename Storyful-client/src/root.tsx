b2ace55c42d2a579276ea48167db5c9ef99e83a6
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-24 09:00:45 +0200

    Fetches test data and sets stories state to that

--- Storyful-client/src/root.tsx
+++ Storyful-client/src/root.tsx
@@ -5,17 +5,24 @@ import { useEffect, useState } from "react";
 import RootHeader from "./components/layout/RootHeader/RootHeader";
 import BottomTabNavigator from "./components/navigation/BottomTabNavigator/BottomTabNavigator";
 import { fetchAllStoriesFromDatabase } from "./utils/helpers";
+import { fetchAllStoriesFromDatabaseTest } from "./utils/__tests__/helpers";
 
 const Root = () => {
   const [stories, setStories] = useState<any>();
 
   useEffect(() => {
-    fetchAllStoriesFromDatabase()
+    fetchAllStoriesFromDatabaseTest()
       .then((response: any) => {
         console.log(response);
         setStories(response.data);
       })
       .catch((error) => console.log(error));
+    // fetchAllStoriesFromDatabase()
+    //   .then((response: any) => {
+    //     console.log(response);
+    //     setStories(response.data);
+    //   })
+    //   .catch((error) => console.log(error));
   }, []);
 
   return (
