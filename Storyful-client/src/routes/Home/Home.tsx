361227f7843d54f3cd3a8537073222ac1f4e9222
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-24 09:03:41 +0200

    No longer going to post images

--- Storyful-client/src/routes/Home/Home.tsx
+++ Storyful-client/src/routes/Home/Home.tsx
@@ -8,26 +8,7 @@ import ActionIcon from "../../assets/Categories/ActionIcon";
 import axios from "axios";
 import bookshelf from "../../assets/profile/bookshelf.png";
 
-const Home = () => {
-  const postImage = async () => {
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        source:
-          "https://www.shutterstock.com/image-vector/blue-watercolor-paint-stroke-background-600w-1725048721.jpg",
-      }),
-    };
-
-    const response = await fetch(
-      `https://freeimage.host/api/1/upload`,
-      options
-    );
-
-    console.log(response);
-  };
+const Home: React.FC = () => {
   return (
     <section className="home-container">
       <Searchbar />
@@ -41,13 +22,6 @@ const Home = () => {
           icon={<RomanceIcon colour="#000000" />}
         />
       </article>
-      <button
-        onClick={() => {
-          postImage();
-        }}
-      >
-        Post image
-      </button>
     </section>
   );
 };
