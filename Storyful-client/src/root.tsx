565d480cb5c552c4ccf15f7537d24f01d697c05b
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-24 16:45:29 +0200

    Wrapped stories context provider around outlet

--- Storyful-client/src/root.tsx
+++ Storyful-client/src/root.tsx
@@ -5,30 +5,15 @@ import { useEffect, useState } from "react";
 import RootHeader from "./components/layout/RootHeader/RootHeader";
 import BottomTabNavigator from "./components/navigation/BottomTabNavigator/BottomTabNavigator";
 import { fetchAllStoriesFromDatabase } from "./utils/helpers";
-import { fetchAllStoriesFromDatabaseTest } from "./utils/__tests__/helpers";
+import StoriesContextProvider from "./services/ContextProviders/StoriesContextProvider";
 
 const Root = () => {
-  const [stories, setStories] = useState<any>();
-
-  useEffect(() => {
-    fetchAllStoriesFromDatabaseTest()
-      .then((response: any) => {
-        console.log(response);
-        setStories(response.data);
-      })
-      .catch((error) => console.log(error));
-    // fetchAllStoriesFromDatabase()
-    //   .then((response: any) => {
-    //     console.log(response);
-    //     setStories(response.data);
-    //   })
-    //   .catch((error) => console.log(error));
-  }, []);
-
   return (
     <>
       <RootHeader />
-      <Outlet context={[stories, setStories]} />
+      <StoriesContextProvider>
+        <Outlet />
+      </StoriesContextProvider>
       <BottomTabNavigator />
     </>
   );
