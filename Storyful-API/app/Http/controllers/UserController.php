e43f0566c6e5c1bb2be05b9c105a469a08b6f5ea
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-25 19:03:59 +0200

    Function authenticates user and return jwt token that expires in three days

--- Storyful-API/app/Http/controllers/UserController.php
+++ Storyful-API/app/Http/controllers/UserController.php
@@ -1,3 +1,5 @@
+<?php
+
 namespace App\Http\Controllers;
 use Psr\Container\ContainerInterface;
 use Psr\http\Message\ResponseInterface as Response;
@@ -17,5 +19,28 @@ class UserController
         $rows = $users->fetchAll();
         $response->getBody()->write(json_encode($rows));
         return $response->withHeader('Content-Type', 'application/json');
+    }
+    public function authenticate(Request $request, Response $response)
+    {
+        $userPayload = $request->getBody()->getContents();
+        $parsedUserPayload = json_decode($userPayload, true);
+
+        $user = authenticateUser($userPayload, $userPayload);
+        if (!$user) {
+            $response = $response
+                ->withHeader("Content-Type", "application/json")
+                ->withStatus(401);
+            $response->getBody()->write(array("error" => "Invalid credentials"));
+            return $response;
+        } else {
+            $jwtPayload = array(
+                "sub" => "602e44f96ee4c",
+                "username" => "guest.test",
+                "exp" => time() + 259200 // Token expires in 3 days
+            );
+            $jwt = \Firebase\JWT\JWT::encode($jwtPayload, \App\Interfaces\KeyInterface::JWT_KEY, 'HS256');
+            $response->getBody()->write(json_encode(array("jwt" => $jwt)));
+            return $response->withHeader("Content-Type", "application/json");
+        }
     }
 }
