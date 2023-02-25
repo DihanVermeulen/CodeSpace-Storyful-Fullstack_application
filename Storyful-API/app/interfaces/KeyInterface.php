55dff5cbcf8c247fcf459702eb55c8ebd82db7cb
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-25 19:01:08 +0200

    Interface that contains secret key only for development

---
+++ Storyful-API/app/interfaces/KeyInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+/* This is only for testing purposes, the key to the JWT is stored here */
+
+namespace App\Interfaces;
+/**
+ * JWT Key interface
+ */
+interface KeyInterface {
+    const JWT_KEY = "Cj23DreakOYUz5d3WFBisOtchs6VWS9LzjZCcKY1UhRSbvT1oAYK4QZNT6x3CXVXWhKVbML3kOqLxEx";
+}
