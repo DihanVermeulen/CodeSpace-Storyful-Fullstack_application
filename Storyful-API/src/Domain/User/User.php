8f49ca3dcdb3116eb3d04b48015053ed52edc6e3
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-25 19:37:57 +0200

    Moved from app/models

---
+++ Storyful-API/src/Domain/User/User.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\Domain\User;
+
+use JsonSerializable;
+
+class User implements JsonSerializable
+{
+    private ?int $id;
+    private string $username;
+    private string $email;
+    private string $password;
+
+    public function __construct(?int $id, string $username, string $email, string $password)
+    {
+        $this->id = $id;
+        $this->username = strtolower($username);
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    #[\ReturnTypeWillChange]
+    public function jsonSerialize(): array
+    {
+        return [
+            'id' => $this->id,
+            'username' => $this->username,
+            'email' => $this->email,
+            'password' => hashPassword($this->password),
+        ];
+    }
+}
