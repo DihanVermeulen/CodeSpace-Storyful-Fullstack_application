e23b4f3255fbb0d0903f16477668bf8dd63905c7
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-25 07:30:18 +0200

    Sets stories inside of provider

--- Storyful-client/src/services/ContextProviders/StoriesContextProvider.tsx
+++ Storyful-client/src/services/ContextProviders/StoriesContextProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useMemo, useState } from "react";
+import React, { createContext, useEffect, useMemo, useState } from "react";
 import { IStories, storiesContextType } from "../../@types/stories";
 import { fetchAllStoriesFromDatabaseTest } from "../../utils/helpers";
 
@@ -7,14 +7,17 @@ export const StoryDataContext = createContext<storiesContextType | null>(null);
 const StoriesContextProvider = ({ children }: any) => {
   const [stories, setStories] = useState<IStories[]>([]);
 
-  const fetchStories = async () => {
-    console.log("Fetching data...");
-    const jsonData = await fetchAllStoriesFromDatabaseTest();
-    console.log("json data: ", jsonData);
-    setStories(jsonData);
-  };
+  useEffect(() => {
+    const fetchStories = async () => {
+      console.log("Fetching data...");
+      const jsonData = await fetchAllStoriesFromDatabaseTest();
+      console.log("json data: ", jsonData);
+      setStories(jsonData);
+    };
+    fetchStories();
+  }, []);
 
-  const memoizedValue = useMemo(() => ({ stories, fetchStories }), [stories]);
+  const memoizedValue = useMemo(() => ({ stories }), [stories]);
 
   return (
     <StoryDataContext.Provider value={memoizedValue}>
