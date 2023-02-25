b732da6279dcf0778a1ffbcbaf9796692da1df2f
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-25 19:14:02 +0200

    Middleware for JWT authentication
    /users path is protected so that not anyone can access it

--- Storyful-API/app/middleware.php
+++ Storyful-API/app/middleware.php
@@ -1,6 +1,8 @@
 <?php
 
 use Slim\App;
+use Tuupola\Middleware\JwtAuthentication;
+use Psr\Http\Message\ResponseInterface as Response;
 
 return function(App $app) {
     $setting = $app->getContainer()->get('settings');
@@ -10,6 +12,22 @@ return function(App $app) {
         $setting['logErrors'],
         $setting['logErrorDetails']
     );
+
+    $app->add(new JwtAuthentication([
+        "path" => "/",
+        "algorithm" => ["HS256"],
+        "ignore" => ["/users/authenticate", "/stories"],
+        "secret" => \App\Interfaces\KeyInterface::JWT_KEY,
+        "error" => function (Response $response, $arguments) {
+            $data = array("error" => "Invalid token");
+            $response = $response->withHeader("Content-Type", "application/json");
+            $response = $response->withStatus(401);
+            $response->getBody()->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+            return $response;
+        },
+    ]));
+
+    $app->addBodyParsingMiddleware();
 }
 
 ?>
