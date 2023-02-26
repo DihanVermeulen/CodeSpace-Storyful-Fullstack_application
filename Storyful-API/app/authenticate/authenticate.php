f8b225bfce5958951ef2946cbe4b3b9e19020a19
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-26 15:00:21 +0200

    Returns boolean

--- Storyful-API/app/authenticate/authenticate.php
+++ Storyful-API/app/authenticate/authenticate.php
@@ -18,21 +18,19 @@ class Authenticate
      * @param string $email email used to authenticate
      * @param string $password password used to authenticate
      */
-    function authenticateUser(string $email, string $password)
+    function authenticateUser(string $email, string $password): bool
     {
         $db = $this->container->get('db');
 
         $statement = $db->prepare("SELECT * FROM users WHERE email = ?");
         $statement->execute([$email]);
         $rows = $statement->fetchAll(\PDO::FETCH_ASSOC);
-
         if (count($rows) > 0) {
             $hashed_password = $rows[0]['password'];
             $verify_password = password_verify($password, $hashed_password);
             return $verify_password;
         } else {
-            // no data was found
-            return false;
+            return false; // no data was found
         }
     }
 }
