9d170c73b55b5b47a83dea7e604b21046f0f71d1
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-26 15:00:09 +0200

    Added access control in routes file for separation of concerns

--- Storyful-API/app/routes.php
+++ Storyful-API/app/routes.php
@@ -6,6 +6,18 @@ use App\Http\Controllers\StoryController;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function(App $app) {
+    // enable CORS
+    $app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+    });
+
+    $app->add(function ($request, $handler) {
+        $response = $handler->handle($request);
+        return $response->withHeader('Access-Control-Allow-Origin', '*')
+                    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+                    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    });
+
     $app->group('/users', function (Group $group) {
         $group->get('', [UserController::class, 'findAll']);
         $group->get('/{id}', [UserController::class, 'findUserByID']); 
