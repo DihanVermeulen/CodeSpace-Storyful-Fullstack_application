7cf6078cf1b9dd1deddf74c15e5537f08a40a7e7
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-27 11:52:36 +0200

    Updated

--- Storyful-API/app/routes.php
+++ Storyful-API/app/routes.php
@@ -13,9 +13,10 @@ return function(App $app) {
 
     $app->add(function ($request, $handler) {
         $response = $handler->handle($request);
-        return $response->withHeader('Access-Control-Allow-Origin', '*')
-                    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-                    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+        return $response
+                ->withHeader('Access-Control-Allow-Origin', '*')
+                ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+                ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     });
 
     $app->group('/users', function (Group $group) {
