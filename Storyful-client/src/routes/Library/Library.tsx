704def67a2265073ca45473c13dcc5ee5a865820
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-27 14:36:06 +0200

    If user is authenticated then library shows otherwise login

--- Storyful-client/src/routes/Library/Library.tsx
+++ Storyful-client/src/routes/Library/Library.tsx
@@ -1,11 +1,29 @@
+import { useContext } from "react";
+import { AuthContextType } from "../../@types/auth";
 import Searchbar from "../../components/form/Searchbar/Searchbar";
+import { AuthContext } from "../../services/ContextProviders/AuthContextProvider";
 import "./Library.css";
 
-const Library = () => (
-  <section className="library-container">
-    <h1 className="library-heading">Library</h1>
-    <Searchbar />
-  </section>
-);
+const Library = () => {
+  const { authenticate, isAuthenticated } = useContext(
+    AuthContext
+  ) as AuthContextType;
+
+  return (
+    <div>
+      {isAuthenticated && (
+        <section className="library-container">
+          <h1 className="library-heading">Library</h1>
+          <Searchbar />
+        </section>
+      )}
+      {!isAuthenticated && (
+        <section className="library-not-authenticated">
+          <p>Please login to see your library</p>
+        </section>
+      )}
+    </div>
+  );
+};
 
 export default Library;
