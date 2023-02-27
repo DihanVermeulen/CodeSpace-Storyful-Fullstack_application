5a7b1f658f0b44fd01fe864eccad42c1ccfe3c37
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-27 11:50:36 +0200

    This is where the Auth context hook is placed

---
+++ Storyful-client/src/services/ContextProviders/AuthContextProvider.tsx
@@ -0,0 +1,88 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import axiosInstance from "../axios/axios";
+import { IUser, IUserLogin, AuthContextType, IToken } from "../../@types/auth";
+import jwt_decode from "jwt-decode";
+
+export const AuthContext = createContext<AuthContextType>({
+  user: null,
+  isAuthenticated: false,
+  authenticate: async (userLoginInformation: IUserLogin) => {},
+  logout: () => {},
+  JWTToken: null,
+});
+
+const AuthContextProvider = ({ children }: any) => {
+  const [user, setUser] = useState<IUser>({
+    id: null,
+    username: null,
+    email: null,
+  });
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [JWTToken, setJWTToken] = useState<IToken | null>(null);
+
+  const checkIfTokenIsValid = (token: string) => {
+    if (token) {
+      const decodedToken: any = jwt_decode<IToken>(token);
+      const { exp } = decodedToken;
+      if (Date.now() >= exp * 1000) {
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  };
+
+  const authenticate = async (userLoginInformation: IUserLogin) => {
+    await axiosInstance
+      .post<{ jwt: string }>("/users/authenticate", userLoginInformation)
+      .then((response) => {
+        console.log(
+          "Response within AuthContextProvider: authenticate: ",
+          response
+        );
+        console.log(
+          "Token within AuthContextProvider: authenticate: ",
+          response.data.jwt
+        );
+        localStorage.setItem("token", JSON.stringify(response.data.jwt));
+      })
+      .catch((error) =>
+        console.log(
+          "!!!Error within AuthContextProvider: authenticate: ",
+          error
+        )
+      );
+  };
+
+  const logout = () => {
+    console.trace("Log out is trigged");
+  };
+
+  useEffect(() => {
+    const initializeAuth = async () => {
+      if (
+        checkIfTokenIsValid(JSON.parse(localStorage.getItem("token") as string))
+      ) {
+        setIsAuthenticated(true);
+        const token = JSON.parse(localStorage.getItem("token") as string);
+        const decodedToken = jwt_decode<IToken>(token);
+        setJWTToken(decodedToken);
+      } else {
+        logout();
+      }
+    };
+    initializeAuth();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ user, authenticate, isAuthenticated, logout, JWTToken }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
