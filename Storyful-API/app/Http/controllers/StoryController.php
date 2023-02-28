07c955a628e59d5f67d999ce47d7bdb05c6aa441
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-28 13:08:22 +0200

    Takes the id and returns text file containing story

--- Storyful-API/app/Http/controllers/StoryController.php
+++ Storyful-API/app/Http/controllers/StoryController.php
@@ -25,4 +25,13 @@ class StoryController
             ->withHeader('Content-Type', 'application/json')
             ->withHeader('Access-Control-Allow-Origin', '*');
     }
+
+    public function getStoryDocument(Response $response, $id)
+    {
+        $file_data = file_get_contents(__DIR__ . "/./../../../public/data/$id.txt");
+        $response->getBody()->write($file_data);
+        return $response
+            ->withHeader('Content-Type', 'text/plain')
+            ->withHeader('Content-Disposition', "attachment; filename=$id.txt");
+    }
 }
