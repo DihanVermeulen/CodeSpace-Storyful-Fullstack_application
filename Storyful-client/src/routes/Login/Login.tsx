3a1181e6d4d7a04c05e4a85e78becb3b3a9aed3c
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-28 06:53:40 +0200

    Login credentials changes on change event

--- Storyful-client/src/routes/Login/Login.tsx
+++ Storyful-client/src/routes/Login/Login.tsx
@@ -1,14 +1,22 @@
-import { ReactElement, useContext } from "react";
+import { ReactElement, useContext, useState } from "react";
 import { Link } from "react-router-dom";
-import { AuthContextType } from "../../@types/auth";
+import { AuthContextType, IUserLogin } from "../../@types/auth";
 import { AuthContext } from "../../services/ContextProviders/AuthContextProvider";
 import "./Login.css";
 
 export const Login: React.FC = (): ReactElement => {
   const { authenticate } = useContext(AuthContext) as AuthContextType;
+  const [loginCredentials, setLoginCredentials] = useState<IUserLogin | null>({
+    email: null,
+    password: null,
+  });
 
   const handelSubmit = (event: any) => {
     event.preventDefault();
+    authenticate({
+      email: loginCredentials?.email,
+      password: loginCredentials?.password,
+    });
   };
 
   return (
@@ -21,11 +29,29 @@ export const Login: React.FC = (): ReactElement => {
         <h1>Login</h1>
         <div className="login-container-input-group">
           <label htmlFor="email">email</label>
-          <input type="email" name="email" />
+          <input
+            type="email"
+            name="email"
+            onChange={(event) =>
+              setLoginCredentials((prevState: any) => ({
+                ...prevState,
+                email: event.target.value,
+              }))
+            }
+          />
         </div>
         <div className="login-container-input-group">
           <label htmlFor="password">password</label>
-          <input type="password" name="password" />
+          <input
+            type="password"
+            name="password"
+            onChange={(event) =>
+              setLoginCredentials((prevState: any) => ({
+                ...prevState,
+                password: event.target.value,
+              }))
+            }
+          />
         </div>
         <button
           type="submit"
