df7c7ee110624d1c8e197d2da9c312bba2272255
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-02-28 12:14:45 +0200

    Moved stories context provider to main

--- Storyful-client/src/root.tsx
+++ Storyful-client/src/root.tsx
@@ -1,20 +1,17 @@
-// Root components are placed here.
+/* Root components are placed here. */
 
 import { Outlet } from "react-router-dom";
-import { useEffect, useState } from "react";
 import RootHeader from "./components/layout/RootHeader/RootHeader";
 import BottomTabNavigator from "./components/navigation/BottomTabNavigator/BottomTabNavigator";
-import { fetchAllStoriesFromDatabase } from "./utils/helpers";
 import StoriesContextProvider from "./services/ContextProviders/StoriesContextProvider";
 
-const Root = () => (
-  <>
-    <RootHeader />
-    <StoriesContextProvider>
+const Root = () => {
+  return (
+    <>
+      <RootHeader />
       <Outlet />
-    </StoriesContextProvider>
-    <BottomTabNavigator />
-  </>
-);
-
+      <BottomTabNavigator />
+    </>
+  );
+};
 export default Root;
