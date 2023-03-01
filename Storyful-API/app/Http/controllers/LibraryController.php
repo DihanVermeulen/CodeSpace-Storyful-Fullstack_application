6915214dfec014b3566f00d03016328f9055714e
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-03-01 08:44:28 +0200

    Library controller that controls library routes

---
+++ Storyful-API/app/Http/controllers/LibraryController.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use PDOException;
+use Psr\Container\ContainerInterface;
+use Psr\http\Message\ResponseInterface as Response;
+use Psr\http\Message\RequestInterface as Request;
+
+class LibraryController
+{
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+
+    public function getUserLibrary(Response $response, $id)
+    {
+        $db = $this->container->get('db');
+
+        $library = $db->query("SELECT * FROM library WHERE user_id = '{$id}'");
+        $rows = $library->fetchAll();
+
+        $response->getBody()->write(json_encode($rows));
+        return $response
+            ->withHeader("Content-Type", "application/json");
+    }
+
+    public function addStoryToLibrary(Request $request, Response $response)
+    {
+        $request_data = $request->getBody()->getContents();
+        $parsed_data = json_decode($request_data, true);
+        $db = $this->container->get('db');
+        $user_id = $parsed_data['user_id'];
+        $story_id = $parsed_data['story_id'];
+        $status = $parsed_data['status'];
+
+
+        try {
+            $statement = $db->prepare("INSERT INTO library (user_id, story_id, status) VALUES(?,?,?)");
+            $statement->execute([$user_id, $story_id, $status]);
+            $response->getBody()->write(json_encode(array(
+                "message" => "success"
+            )));
+            return $response
+                ->withHeader("Content-Type", "application/json");
+        } catch (PDOException $error) {
+            if ($error->getCode() == "23000") {
+                // This is a database constraint violation, indicating a duplicate row
+                $response->getBody()->write(json_encode(array("error" => "Duplicate row")));
+            } else {
+                // Other database error occurred
+                $response->getBody()->write(json_encode(array("error" => $error)));
+            }
+            return $response;
+        }
+    }
+}
