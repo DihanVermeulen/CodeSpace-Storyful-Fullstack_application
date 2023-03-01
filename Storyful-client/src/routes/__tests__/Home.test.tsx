63acdda7a1b4df8f375f0ea9fb704f570765220b
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-03-01 07:13:13 +0200

    Updated

--- Storyful-client/src/routes/__tests__/Home.test.tsx
+++ Storyful-client/src/routes/__tests__/Home.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import { describe, it } from "vitest";
-import { fetchAllStoriesFromDatabase } from "../../utils/helpers";
 import Home from "../Home/Home";
 
 describe("Home", () => {
