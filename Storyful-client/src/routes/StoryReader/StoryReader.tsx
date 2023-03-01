0dd655da2d9649366d8eed2eaa611205fef5cefc
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2023-03-01 14:04:28 +0200

    When status updates it sends put request

--- Storyful-client/src/routes/StoryReader/StoryReader.tsx
+++ Storyful-client/src/routes/StoryReader/StoryReader.tsx
@@ -1,11 +1,14 @@
 import { useContext, useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { storiesContextType } from "../../@types/stories";
+import { AuthContextType } from "../../@types/auth";
+import { LibraryContextType, storiesContextType } from "../../@types/stories";
 import BackIcon from "../../assets/TopNavigator/BackIcon";
 import MoreIcon from "../../assets/TopNavigator/MoreIcon";
 import Dropdown from "../../components/form/Dropdown/Dropdown";
 import axiosInstance from "../../services/axios/axios";
+import { LibraryContext } from "../../services/ContextProviders/LibraryContextProvider";
 import { StoryDataContext } from "../../services/ContextProviders/StoriesContextProvider";
+import { AuthContext } from "../../services/ContextProviders/AuthContextProvider";
 import "./StoryReader.css";
 
 interface IStoryInformation {
@@ -18,27 +21,40 @@ interface IStoryInformation {
 const StoryReader = () => {
   const [storyInformation, setStoryInformation] = useState<IStoryInformation>();
   const [documentData, setDocumentData] = useState<string>();
-  const [option, setOption] = useState<number | null>(0);
+  const [option, setOption] = useState<number | null>(null);
+  const [storyIsInLibrary, setStoryIsInLibrary] = useState<boolean>(false);
   const { stories } = useContext(StoryDataContext) as storiesContextType;
+  const { JWTToken } = useContext(AuthContext) as AuthContextType;
+  const { library } = useContext(LibraryContext) as LibraryContextType;
   const location = useLocation();
   const navigate = useNavigate();
+
+  const checkIfStoryIsInLibrary = (id: number): any => {
+    console.log("checking if story is in library...");
+    library.map((story) => {
+      console.log("library story: ", story);
+      if (story.story_id == id) {
+        console.log("Story is in library");
+        console.log("Story status: ", story.status);
+        setOption(story.status);
+        setStoryIsInLibrary(true);
+        return true;
+      } else {
+        return;
+      }
+    });
+  };
 
   useEffect(() => {
     const searchParams = new URLSearchParams(location.search);
     const id: any = searchParams.get("story-id");
-    console.log("id: ", id);
-    console.log(stories);
     const getStory = async () => {
       stories.map((story) => {
         if (story.id == id) {
           setStoryInformation(story);
-          console.log(story);
         }
       });
     };
-    if (id) {
-      getStory();
-    }
 
     const fetchDocument = async () => {
       const response: any = await axiosInstance.get(`stories/document/${id}`);
@@ -46,12 +62,52 @@ const StoryReader = () => {
       setDocumentData(data);
     };
     if (id) {
+      getStory();
       fetchDocument();
     }
   }, [location.search, stories]);
 
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const id: any = searchParams.get("story-id");
+    if (library) {
+      checkIfStoryIsInLibrary(id);
+    }
+  }, [library]);
+
   const handleBackNavigate = () => {
     navigate(-1);
+  };
+
+  const handleOptionChange = async (id: any, status: number) => {
+    const searchParams = new URLSearchParams(location.search);
+    id = searchParams.get("story-id");
+
+    const userID = JWTToken?.id;
+
+    const token = JSON.parse(localStorage.getItem("token") as string);
+    console.log("From inside handle option change: ", token);
+    if (token) {
+      const headers = {
+        Authorization: `Bearer ${token}`,
+      };
+
+      const updatedData = {
+        story_id: id,
+        status: status,
+      };
+
+      const response = await axiosInstance.put(
+        `library/${userID}`,
+        updatedData,
+        {
+          headers: headers,
+        }
+      );
+
+      const { data } = response;
+      console.log(data);
+    }
   };
 
   return (
@@ -63,7 +119,12 @@ const StoryReader = () => {
         >
           <BackIcon colour="#ffffff" width="30px" height="30px" />
         </button>
-        <Dropdown options={["Planned", "Reading", "Completed"]} selectedOption={option != null ? option : null}>
+        <Dropdown
+          options={["Planned", "Reading", "Completed"]}
+          selectedOption={option != null ? option : null}
+          storyIsInLibrary={storyIsInLibrary}
+          handleOptionChange={handleOptionChange}
+        >
           <button className="storyreader-top-navbar-button">
             <MoreIcon colour="#ffffff" width="30px" height="30px" />
           </button>
